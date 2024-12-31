421864f669f7f88aea66413fce93b8c0fe7a7265
orbachoror <orbachoror@cs.colman.ac.il>
2024-12-31 20:37:38 +0200

    new studdent

--- app/src/main/java/com/idz/assignment2studentsapp/Model/Student.kt
+++ app/src/main/java/com/idz/assignment2studentsapp/Model/Student.kt
@@ -3,6 +3,7 @@ package com.idz.assignment2studentsapp.Model
 data class Student(
     val name:String,
     val id:String,
-    val avatarUrl:String,
+    val phone:String = "",
+    val address:String = "",
     var isChecked:Boolean
 )
